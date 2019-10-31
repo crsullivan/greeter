04978493cc6171bdcaae6a85b54200e9a4360b78
Connor R. Sullivan <crsullivan@protonmail.com>
2019-10-31 10:22:29 -0700

    env port and node script

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
+const port = process.env.PORT || 4000;
 server.listen(4000, () => {
   console.log('\n Server running on port 4000 \n');
 });
